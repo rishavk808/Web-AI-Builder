3c751d80083b61b7ddf000662779debf34f16398
Rishav Kumar <rishavk808@gmail.com>
2026-05-23 15:39:58 +0530

    Created the frontend layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React, { useState } from 'react'
+import "./App.css"
+import Navbar from './components/Navbar'
+import { MdArrowUpward } from "react-icons/md";
+import { ImNewTab } from "react-icons/im";
+import { IoMdDownload } from "react-icons/io";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () =>{
+    const [prompt, setPrompt] = useState("");
+    return (
+        <>
+          <Navbar/>
+          <div className = "container">
+            <h3 className = 'text-[30px] font-[700]'>Turn Ideas into Websites Instantly with <span className = "bg-gradient-to-br from-violet-400  to-purple-600 bg-clip-text text-transparent">WebAI</span></h3>
+            <p className = 'mt-2 text-[16px] text-[#b3b3b3]'>Describe your website idea.</p>
+            <div className="inputBox">
+                <textarea onChange = {(e)=>{setPrompt(e.target.value)}} value={prompt} placeholder = 'Tell me about your website in detail'></textarea>
+                {
+                  prompt !== "" ?
+                  <> 
+                   <i className = 'sendIcon text-[20px] w-[30px] h-[30px] flex items-center justify-center bg-[#9933ff] rounded-[50%] transition-all duration-300 hover:opacity-[.8]'><MdArrowUpward /></i>
+                  </> : ""
+                }
+            </div>
+           <p className = 'text-[20px] font-[700] mt-[10vh]'>Your AI-Generated Website will appear here.</p>
+           <div className = "preview">
+            <div className="header w-full h-[70px]">
+              <h3 className = 'font-bold text-[16px]'>Live Preview</h3>
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+               <div className="icons flex items-center gap-[15px]">
+               <div className= "icon !w-[auto] !p-[12px] flex items-center gap-[10px]">Open in new tab <ImNewTab /></div>
+               <div className= "icon !w-[auto] !p-[12px] flex items-center gap-[10px]">Download<IoMdDownload /></div>
+            </div>
+            </div>
+           
+           </div>
+          </div>
+        </>
+    )
 }
 
 export default App
